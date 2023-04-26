ffdf20c72be076b529afe4511ce333ca5b9800d2
Axel Bestory <bstory@MacBook-Pro-de-Axel.local>
2023-04-26 13:39:30 +0200

    feat(archetypes):get classes

--- src/views/ClassesView.vue
+++ src/views/ClassesView.vue
@@ -2,7 +2,52 @@
   <div class="about">
     <h1>Manage Classes</h1>
   </div>
+  <div class="archetypes-items">
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+
+                </tr>
+                <tr v-for="(archetype, index) in archetypes" :key="index" class="archetype-item">
+                    <td>{{ archetype.id }}</td>
+                    <td>{{ archetype.name }}</td>
+                    <td>
+                        <button>Edit</button>
+                        <button @click="deleteArchetype(archetype.id)">Delete</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
 </template>
+
+<script>
+export default {
+    name: "ClassesView",
+    data() {
+        return {
+          archetypes: String
+        }
+    },
+    methods: {
+        async getArchetypes() {
+            const data = await fetch('http://localhost:3000/classes')
+            this.archetypes = await data.json()
+        },
+        async deleteArchetype(archetypeId) {
+            await fetch(`http://localhost:3000/classes/${archetypeId}`, {
+                method: 'DELETE'
+            })
+        }
+    },
+    mounted() {
+        this.getArchetypes()
+    },
+    updated() {
+        this.getArchetypes()
+    }
+}
+</script>
 
 <style>
 </style>
